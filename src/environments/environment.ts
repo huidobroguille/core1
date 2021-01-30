ab1e7533777459163b21f98cfed02508c3bb976c
huidobroguille <guille.g.huidobro@gmail.com>
2021-01-30 20:02:50 -0300

    primer commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA6b-joHG55KN1Y78eqoflA_4fXqfUuMls",
+    authDomain: "core1-d11a9.firebaseapp.com",
+    databaseURL: "https://core1-d11a9-default-rtdb.firebaseio.com",
+    projectId: "core1-d11a9",
+    storageBucket: "core1-d11a9.appspot.com",
+    messagingSenderId: "358022838257",
+    appId: "1:358022838257:web:79e38c970095877dbdb650",
+    measurementId: "G-SN5JGP756C"
+  }
+};
